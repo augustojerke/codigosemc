5220309ba124645f0e6d7a7e03a998cd6c10cb73
Augusto <augustowieles@gmail.com>
2022-10-05 01:46:09 -0300

    jogo da velha finalizado

--- jogo_da_velha.c
+++ jogo_da_velha.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <windows.h>
 
 void abertura()
 {
@@ -17,6 +18,23 @@ void zerarmatriz (char velha[3][3])
 			velha[i][j]=' ';		
 		}
 	}
+}
+
+int verificarvelha (char velha [3][3])
+{
+	int flagvelha = 1;
+	
+	for(int i = 0; i<3; i++)
+    	{
+    		for(int j = 0; j<3; j++)
+    		{
+    			if(velha[i][j]==' ')
+    			{
+    				flagvelha = 0;
+				}
+			}
+		}
+	return flagvelha;
 }
 
 void mostrarvelha (char velha[3][3])
@@ -40,119 +58,180 @@ void mostrarvelha (char velha[3][3])
 	
 }
 
-char jogadadojogador(int *jog, char jogada)
+void jogadadojogador(int *jog, int jogada, int jogada2, char velha[3][3])
 {
-	printf("\nJogador %d, sua vez! \n", *jog);
-    fflush(stdin);
-    scanf(" %c",&jogada);
-    (*jog)++;
-    if((*jog)==3)
+	if((*jog)==3)
     {
     	(*jog)=1;
 	}
-	else if((*jog)==1)
+	printf("\nJogador %d, sua vez! (Digite linha e coluna respectivamente) \n", *jog);
+    printf("Linha: \n");
+    fflush(stdin);
+    scanf("%d",&jogada);
+    printf("Coluna: \n");
+    fflush(stdin);
+    scanf("%d",&jogada2);
+	if((*jog)==1)
 	{
-		velha[]
+		if(velha[jogada-1][jogada2-1]!='X' && velha[jogada-1][jogada2-1]!='O')
+		velha[jogada-1][jogada2-1]='X';
+		else
+		{
+			printf("Por favor, jogue um local vazio!\n");
+			(*jog)=0;
+			Sleep(3000);
+		}
+		
 	}
-	
-	return jogada;
+	else if((*jog)==2)
+	{
+		if(velha[jogada-1][jogada2-1]!='X' && velha[jogada-1][jogada2-1]!='O')
+		velha[jogada-1][jogada2-1]='O';
+		else
+		{
+			printf("Por favor, jogue um local vazio!\n");
+			(*jog)=1;
+			Sleep(3000);
+		}
+		
+	}
+	(*jog)++;
 }
 
-int deuvelha (int *soma, char velha[3][3])
+int deuvelha (int *soma, char velha[3][3],int *quemvenceu)
 {
 	//Verificação Soma de O
     			if(velha[0][0]=='O' && velha[1][0]=='O' && velha[2][0]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[0][1]=='O' && velha[1][1]=='O' && velha[2][1]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[0][2]=='O' && velha[1][2]=='O' && velha[2][2]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[0][0]=='O' && velha[0][1]=='O' && velha[0][2]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[1][0]=='O' && velha[1][1]=='O' && velha[1][2]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[2][0]=='O' && velha[2][1]=='O' && velha[2][2]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[0][0]=='O' && velha[1][1]=='O' && velha[2][2]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				else if(velha[0][2]=='O' && velha[1][1]=='O' && velha[2][0]=='O')
     			{
     				*soma = 1;
+    				*quemvenceu = 2;
 				}
 				
 	//Verificação Soma de X
 				if(velha[0][0]=='X' && velha[1][0]=='X' && velha[2][0]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[0][1]=='X' && velha[1][1]=='X' && velha[2][1]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[0][2]=='X' && velha[1][2]=='X' && velha[2][2]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[0][0]=='X' && velha[0][1]=='X' && velha[0][2]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[1][0]=='X' && velha[1][1]=='X' && velha[1][2]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[2][0]=='X' && velha[2][1]=='X' && velha[2][2]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[0][0]=='X' && velha[1][1]=='X' && velha[2][2]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
 				else if(velha[0][2]=='X' && velha[1][1]=='X' && velha[2][0]=='X')
     			{
     				*soma = 1;
+    				*quemvenceu = 1;
 				}
-		return soma;
-	
+		return *soma;
+}
+
+void resultado (int soma, char velha[3][3], int quemvenceu)
+{
+	if(soma)
+	{
+		printf("Parabens!! Jogador %d, voce venceu!\n", quemvenceu);
+		mostrarvelha(velha);
+	}
+	else
+	{
+		printf("Deu velha!!\n");
+		mostrarvelha(velha);
+	}
 }
 
 int main ()
 {
-    char velha[3][3], jogada;
+    char velha[3][3];
     int jog = 1;
-    int teste = 0
+    int teste = 0;
     int soma = 0;
     int tentativas = 1;
+    int jogada, jogada2;
+    int quemvenceu;
+    int flagvelha = 1;
     
     zerarmatriz(velha);
     
     do{
-    
+    	
+    	flagvelha = verificarvelha(velha);
+    	if(flagvelha == 1)
+    	{
+    		break;
+		}
+		
 		abertura();
     	mostrarvelha(velha);
-    	jogada = jogadadojogador(&jog, jogada);
-    	soma = deuvelha(&soma, velha);
+    	jogadadojogador(&jog, jogada, jogada2, velha);
+    	soma = deuvelha(&soma, velha, &quemvenceu);
     	tentativas++;
-		
+    	
     	system("cls");
-		
-	}while(!soma || tentavivas == 9);
-    
-        
+    	
+	}while(!soma);
+	
+	resultado(soma, velha, quemvenceu);
+	
 	return 0;
 }
